61ccf51746f1497dfe31e745e36d5ee61e78b0fc
bozzafresco <bozza.fresco@gmail.com>
2015-08-21 15:43:46 +0300

    Правки корзины по методам доставки
    Добавил адрса первозчиков и убрал ноль из цены доставки

--- catalog/language/russian/shipping/free.php
+++ catalog/language/russian/shipping/free.php
@@ -1,4 +1,4 @@
 ﻿<?php
 // Text
-$_['text_title']       = 'Курьерская служба <a href="#">Интайм</a>';
+$_['text_title']       = 'Курьерская служба <a href="http://www.intime.ua/calc/" target="_blank">Интайм</a>';
 $_['text_description'] = 'Доставка Интаймом по тарифу';
